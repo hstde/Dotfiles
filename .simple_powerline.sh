0561aef39a5cc1a40d957966c16600f0290d482f
hstde <kranz.chr@googlemail.com>
2022-02-22 16:01:07 +0100

    added more beautification to .simple_powerline

--- .simple_powerline.sh
+++ .simple_powerline.sh
@@ -1,10 +1,25 @@
-REDBG=`tput setab 1`
-WHITE=`tput setaf 7`
-NC=`tput sgr0`
+RED=52
+WHITE=255
+DARKGRAY=236
+GRAY=240
+LIGHTGRAY=252
+LIGHTBLUE=31
+B=''
+BR=''
+
+ERRORC="$(tput setaf $WHITE; tput setab $RED)"
+NC='\e[0m'
+if [ $EUID == 0 ]; then
+    USERC="$(tput setaf $WHITE; tput setab $RED)"
+else
+    USERC="$(tput setaf $WHITE; tput setab $LIGHTBLUE)"
+fi
+BRANCHC="$(tput setaf $LIGHTGRAY; tput setab $DARKGRAY)"
+DIRC="$(tput setaf $LIGHTGRAY; tput setab $GRAY)"
 
 function nonzero_return() {
 	RETVAL=$?
-	[ $RETVAL -ne 0 ] && echo "${REDBG}${WHITE}$RETVAL "
+	[ $RETVAL -ne 0 ] && echo "$ERRORC $RETVAL "
 }
 
 # get current branch in git repo
@@ -13,47 +28,11 @@ function parse_git_branch() {
     BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 	if [ ! "${BRANCH}" == "" ]
     then
-	    STAT=`parse_git_dirty`
-		echo "[${BRANCH}${STAT}]"
+		echo "${BRANCHC} ${BR} ${BRANCH} "
 	else
 		echo ""
 	fi
 	return $exit_code
 }
 
-# get current status of git repo
-function parse_git_dirty {
-    status=`git status 2>&1 | tee`
-	dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-	untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
-	ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
-	newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-	renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-	deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
-	bits=''
-	if [ "${renamed}" == "0" ]; then
-		bits=">${bits}"
-	fi
-	if [ "${ahead}" == "0" ]; then
-    	bits="*${bits}"
-	fi
-	if [ "${newfile}" == "0" ]; then
-		bits="+${bits}"
-	fi
-	if [ "${untracked}" == "0" ]; then
-		bits="?${bits}"
-	fi
-	if [ "${deleted}" == "0" ]; then
-		bits="x${bits}"
-	fi
-	if [ "${dirty}" == "0" ]; then
-		bits="!${bits}"
-	fi
-	if [ ! "${bits}" == "" ]; then
-    	echo " ${bits}"
-	else
-		echo ""
-	fi
-}
-
-export PS1="\[\033[48;5;6m\]\u:\[\033[48;5;237m\]\`parse_git_branch\` \[\033[38;5;7m\]\w\\$ \`nonzero_return\`${NC} "
+export PS1="${USERC} \u \`parse_git_branch\`${DIRC} \w\ \`nonzero_return\`${NC} "
